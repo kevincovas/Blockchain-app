8eacd79631ea984ce1b32bcd336330da6285cc72
Aitor Garzón <agarzon@ribesalat.com>
2021-09-02 18:01:51 +0200

    aitor First Version calendar con Use Effect y Use State

--- frontend/src/Calendar.jsx
+++ frontend/src/Calendar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
+import React , { useEffect, useState } from 'react';
 
 // Month Translation in Spanish
 const MONTHS = [
@@ -31,6 +31,60 @@ const WEEKDAYS_LONG = [
 
 // Week Days in Spanish
 const WEEKDAYS_SHORT = ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'];
+
+
+
+function Calendar()
+{
+	// Set None day Selected
+	const [state, setState] = useState({selectedDay: null});
+	const [test, setDisabledDays] = useState([new Date(2021, 8, 12)]);
+	
+	  // On click a day, change state
+  function handleDayClick(day, { selected }) {
+   setState({
+      selectedDay: selected ? undefined : day,
+    });
+  }
+	
+	// Init Values
+	useEffect(() => {  }, []);	
+	useEffect(() => { /* Buscaré las citas disponibles ése día */ }, [state.selectedDay]);	
+
+	
+	return ( 
+
+
+<div>
+        <DayPicker
+         
+onDayClick={ handleDayClick }	  
+locale="es"
+      months={MONTHS}
+      weekdaysLong={WEEKDAYS_LONG}
+      weekdaysShort={WEEKDAYS_SHORT}
+      firstDayOfWeek={1}
+ showOutsideDays 	  
+		  selectedDays={state.selectedDay}
+		
+		disabledDays={test}
+		  
+		  
+        />
+
+		<p>{state.selectedDay
+            ? state.selectedDay.toLocaleDateString()
+            : 'Selecciona el día'}</p>
+
+      </div>
+
+
+	);
+}
+
+export default Calendar ;
+
+/*
 
 // Calendar Class
 export default class Calendar extends React.Component {
@@ -75,4 +129,9 @@ locale="es"
       </div>
     );
   }
+  
+  
+  
 }
+
+*/
