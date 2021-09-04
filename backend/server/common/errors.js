57322e475e7cf384bdce1790592bf82da533b292
Kevin Covas Domenech <kevin.covas.91@gmail.com>
2021-09-04 12:17:40 +0200

    Conexion de frontend-server y gestión de errores

---
+++ backend/server/common/errors.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+    constructor(code, message) {
+      super();
+      this.name = "ApiError";
+      this.code = code;
+      this.message = message;
+    }
+  }
+  
+  const errorHandler = (err, req, res, next) => {
+    if (err.name === "ApiError") {
+      const { code, message } = err;
+      return res.status(code).send({ error: message });
+    }
+    console.error(err);
+    res.status(500).send({ error: `Internal Server error (TODO FULLSTACK): ${err}` });
+  };
+
+  module.exports = {
+    ApiError,
+    errorHandler,
+  };
--- backend/server/server.js
+++ backend/server/server.js
@@ -3,14 +3,29 @@ require('dotenv').config(); //Pone las variables del fichero .env en el entorno.
 const express = require("express");
 const cors = require("cors");
 const config = require('./config');
+const morgan = require("morgan");
+const helmet = require("helmet");
+const {errorHandler, ApiError} = require("./common/errors");
 
 const app = express();
 
+//Middlewares
+app.use(helmet());
 app.use(express.json());  //Convierte el body(JSON) en un objeto Javascript
-app.use(cors()); 
+app.use(cors());
+app.use(morgan('dev'));
+
+//Servimos el fichero index.html en la raiz
+app.use(express.static('../../frontend/dist')); 
 
 app.use("/clients", require('./clients'));
 app.use("/users", require('./users'));
+
+//Provocamos un error para cazarlo
+app.get('/error', async (req,res,next) => {
+    next(new ApiError(503, 'Error provocado'));
+})
+app.use(errorHandler);
 
 app.listen(config.SERVER_PORT, () => {
     console.log(`peluqueria_app server on port ${config.SERVER_PORT}`);
