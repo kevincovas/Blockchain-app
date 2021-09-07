e7031dc55d92c9ebcb660497b8cf8947cf54a4c2
AndreacgCoder <comabasosa.g.a@gmail.com>
2021-09-07 21:07:43 +0200

    Login query

---
+++ backend/server/db_users.js
@@ -0,0 +1,109 @@
+const {pool} = require('./db');
+
+const getUsersSQL = `
+    SELECT * FROM users;
+`;
+
+const getUsers = async() => {
+    
+    try {
+        const result = await pool.query(getUsersSQL);
+        //Comprobamos que haya usuarios
+        if(result.rowCount < 1) {
+           return { ok: true, found: false }; //No se han encontrado usuarios
+        }
+        return { ok:true, found: true, data: result.rows}; //Se han encontrado usuarios
+    }catch(e) {
+           return { ok: false, data: e.toString() };
+       }
+}
+
+const getOneUserSQL = `
+    SELECT * FROM users WHERE USE_id = $1;
+`;
+
+const getOneUser = async(USE_id) => {
+    try {
+        const result = await pool.query(getOneUserSQL, [USE_id]);
+        //Comprobamos que el cliente exista
+        if(result.rowCount < 1) {
+           return { ok: true, found: false }; //No se ha encontrado el usuario
+        }
+        return { ok:true, found: true, data: result.rows}; //Se ha encontrado el usuario
+    }catch(e) {
+           return { ok: false, data: e.toString() };
+       }
+}
+
+const newUserSQL = `
+    INSERT INTO users (USE_email, USE_password) VALUES ($1,$2) RETURNING *;
+`;
+
+const newUser = async (USE_email, hashedPassword) => {
+    const result = await pool.query(newUserSQL, [USE_email, hashedPassword]);
+    return result.rows;
+}
+
+const updateUserSQL = `
+    UPDATE users SET  USE_email = $2,
+                      USE_password = $3
+    WHERE USE_id = $1 RETURNING *;  
+`;
+
+const updateUser = async (USE_id,USE_email, USE_password) => {
+    try {    
+        const result = await pool.query(updateUserSQL, [USE_id,USE_email, USE_password]);
+        if(result.rowCount < 1) {
+            return { ok: true, found: false };
+        }
+    return { ok:true, found: true, data: result.rows};
+    } catch(e) {
+        return { ok: false, data: e.toString() };
+    }
+}
+
+
+const deleteUserSQL = `
+DELETE FROM users WHERE USE_id = $1 RETURNING *; 
+`;
+
+const deleteUser = async (USE_id) => {
+    try {
+        const result = await pool.query(deleteUserSQL, [USE_id]);
+        //Comprobamos que se haya eliminado algun usuario
+        if(result.rowCount < 1) {
+           return { ok: true, found: false }; //No se ha eliminado ningun usuario
+        }
+        return { ok:true, found: true, data: result.rows}; //Se ha eliminado el usuario
+       }catch(e) {
+           return { ok: false, data: e.toString() };
+       }
+};
+
+const getUserByEmailSQL = `
+    SELECT USE_password FROM users WHERE USE_email = $1;
+`;
+ 
+const getUserByEmail = async(USE_email,) => {
+    try {
+        const result = await pool.query(getUserByEmailSQL, [USE_email]);
+        
+        //Comprobamos que el usuario existe
+        if(result.rowCount < 1) {
+           return { ok: true, found: false }; //No se ha encontrado el usuario
+        }
+        return { ok:true, found: true, data: result.rows[0]}; //Se ha encontrado el usuario
+    }catch(e) {
+           return { ok: false, data: e.toString() };
+       }
+}
+
+module.exports = {
+    getUsers,
+    getOneUser,
+    newUser,
+    updateUser,
+    deleteUser,
+    getUserByEmail,
+};
+
--- backend/server/users.js
+++ backend/server/users.js
@@ -21,6 +21,39 @@ router.get('/', async (req,res) =>{
     } 
 });
 
+/*Post para hacer el LOGIN */
+router.post("/login", async (req, res) => {
+    const { USE_email, USE_password } = req.body;
+
+    if(!USE_email){
+        return res.status(400).json(errorResult("Missing 'email' field"));
+    }
+    if(!USE_password){
+        return res.status(400).json(errorResult("Missing 'password' field"));
+    }
+    const {ok, found, data} = await db.getUserByEmail(USE_email);
+    //return res.json(okResult(data));
+    if(!ok){ //Si ha habido un error en el servidor
+        return res.status(500).json(errorResult(data));
+    } else if(!found){ //Si no se encuentran usuarios
+        return res
+            .status(400)
+            .json(errorResult(`User doesn't exist`));
+    } else{ //Si hay usuarios
+        const password_db = data.use_password;
+        const passwordMatches = await auth.comparePasswords(USE_password, password_db);
+    
+        if (!passwordMatches) {
+            return res.status(400).json(errorResult(`Wrong email/password combination`));
+        }
+
+        /*const token = auth.createToken(USE_email);*/
+        //res.status(201).send(token);
+        
+        return res.json(okResult(data));
+    } 
+});
+
 //Obtenemos un solo usuario
 router.get('/:USE_id', async (req,res) => {
     const { ok, found, data } = await db.getOneUser(req.params.USE_id);
@@ -53,36 +86,6 @@ router.post("/", async (req,res) => {
         res.status(500).json(errorResult(e.toString()));
     } 
 });
-
-/*Post para hacer el LOGIN */
-router.post("/login", async (req, res) => {
-    const { USE_email, USE_password } = req.body;
-    
-    const {ok, found, USE_password_DB} = await db.getUserByEmail(USE_email);   
-    console.log(`USE_password_DB: ${USE_password_DB}`);
-    if(!USE_email){
-        return res.status(400).json(errorResult("Missing 'email' field"));
-    }
-    if(!USE_password){
-        return res.status(400).json(errorResult("Missing 'password' field"));
-    }
-    if(!ok){ //Si ha habido un error en el servidor
-        return res.status(500).json(errorResult(data));
-    } else if(!found){ //Si no se encuentra el usuario
-        return res
-            .status(400)
-            .json(errorResult(`User doesn't exist`));
-    } else{ //Si existe el usuario
-        const passwordMatches = await auth.comparePasswords(USE_password, USE_password_DB);
-        const token = auth.createToken(USE_email);
-        res.status(201).send(token);
-        if (!passwordMatches) {
-            return res.status(400).json(errorResult(`Wrong email/password combination`));
-        }
-        return res.json(okResult(data));       
-    }    
-});
-
 
 //Actualizamos un usuario
 router.put("/:USE_id" , async(req, res) => {
