e2c3bf1700d44c7b23d1dbae9f3234ca7e9e71f6
Aitor Garzón <agarzon@ribesalat.com>
2021-09-08 20:08:12 +0200

    Bat para el docker

--- initial_project_build.bat
+++ initial_project_build.bat
@@ -1,7 +1,7 @@
 echo off
 cls
 docker-compose up --build -d 
+timeout 5
 docker exec -it backend_db psql -U admin peluqueria -f /docker-entrypoint-initdb.d/setup_db.sql
 docker exec -it backend_db psql -U admin peluqueria -f /docker-entrypoint-initdb.d/dummy_data_db.sql
-echo Hecho con éxito !
-pause
+echo Hecho con éxito ! 
--- stop_docker.bat
+++ stop_docker.bat
@@ -1,7 +1,4 @@
 echo off
 cls
 docker-compose down
-docker rm -f $(docker ps -a -q)
-docker volume rm $(docker volume ls -q)
 echo Docker cerrado !
-pause
