d49fa2bd48f36dba68977177f6eaab17f5111bea
AndreacgCoder <comabasosa.g.a@gmail.com>
2021-09-10 08:44:55 +0200

    Login corregido (rows[0])

--- backend/server/config.js
+++ backend/server/config.js
@@ -7,10 +7,13 @@ const checkEnvVar = (name) => {
     throw new Error("Missing environment variable");
 }
 module.exports = {
+    JWT_SECRET: 'myjwtsecret',
+    JWT_EXPIRATION_TIME: '2h',
     DB_HOST: checkEnvVar('DB_HOST'),
     DB_PORT: checkEnvVar('DB_PORT'),
     DB_USER: checkEnvVar('POSTGRES_USER'),
     DB_PASSWORD: checkEnvVar('POSTGRES_PASSWORD'),
     DB_DATABASE: checkEnvVar('POSTGRES_DB'),
     SERVER_PORT: checkEnvVar('SERVER_PORT'),
+
 };
